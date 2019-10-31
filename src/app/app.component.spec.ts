925e13b2f466d137267828b3cece6f320b491aae
Marcin Konopka <marcin.konopkaa@gmail.com>
2019-10-31 20:36:34 +0100

    Add missing test deps.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MockComponent } from 'ng-mocks';
+import { LayoutComponent } from '@acc-layout/layout/layout.component';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
@@ -12,7 +14,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        MockComponent(LayoutComponent),
       ],
     }).compileComponents();
   }));
