a5e2287d403596dab64c718fcf9605567ad420f2
Marcin Konopka <marcin.konopkaa@gmail.com>
2019-10-31 22:43:02 +0100

    Update article model.

--- src/app/modules/article/models/article.model.ts
+++ src/app/modules/article/models/article.model.ts
@@ -1,7 +1,25 @@
 export interface Article {
   id: string;
   name: string;
-  elements: object;
+  elements: ArticleElements;
   lastModified: string;
 }
 
+export interface ArticleElements {
+  heading: Element<string>;
+  author: Element<string>;
+  body: Elements<string>;
+  mainImage: Element<Image>;
+}
+
+export interface Image {
+  leadImage: { url: string };
+}
+
+export interface Element<T> {
+  value: T;
+}
+
+export interface Elements<T> {
+  values: T[];
+}
