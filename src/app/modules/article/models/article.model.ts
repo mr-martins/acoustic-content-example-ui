007b291ad9b6eede29c621bb0f49095138e659cc
Marcin Konopka <marcin.konopkaa@gmail.com>
2019-11-01 20:58:59 +0100

    Map API data.
    Styled article component.

--- src/app/modules/article/models/article.model.ts
+++ src/app/modules/article/models/article.model.ts
@@ -1,25 +1,13 @@
+export function createArticle(params?: Partial<Article>): Article {
+  return { ...params } as Article;
+}
+
 export interface Article {
   id: string;
   name: string;
-  elements: ArticleElements;
   lastModified: string;
-}
-
-export interface ArticleElements {
-  heading: Element<string>;
-  author: Element<string>;
-  body: Elements<string>;
-  mainImage: Element<Image>;
-}
-
-export interface Image {
-  leadImage: { url: string };
-}
-
-export interface Element<T> {
-  value: T;
-}
-
-export interface Elements<T> {
-  values: T[];
+  imageUrl: string;
+  author: string;
+  body: string[];
+  heading: string;
 }
--- src/app/modules/article/services/articles-data/articles-data.service.ts
+++ src/app/modules/article/services/articles-data/articles-data.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Article } from '../../models/article.model';
+import { Article, createArticle } from '../../models/article.model';
+import { map } from 'rxjs/operators';
+import { get } from 'lodash';
+
+const apiDomain = 'https://my12.digitalexperience.ibm.com';
 
 @Injectable()
 export class ArticlesDataService {
-  private readonly baseApiUrl: string = 'https://my12.digitalexperience.ibm.com/api/859f2008-a40a-4b92-afd0-24bb44d10124/';
+  private readonly baseApiUrl: string = `${apiDomain}/api/859f2008-a40a-4b92-afd0-24bb44d10124/`;
   private readonly articleId: string = 'fa9519d5-0363-4b8d-8e1f-627d802c08a8';
-  private readonly fields: Array<keyof Article> = [
+  private readonly fields: string[] = [
     'id',
     'name',
     'elements',
@@ -18,9 +22,20 @@ export class ArticlesDataService {
   }
 
   fetch(): Observable<Article> {
-    return this.httpClient.get<Article>(
+    return this.httpClient.get<object>(
       `${this.baseApiUrl}delivery/v1/content/${this.articleId}`,
       { params: { fields: this.fields.join(',') } }
-    );
+    ).pipe(map((apiResponse: object) => {
+
+      return createArticle({
+        id: get(apiResponse, 'id'),
+        lastModified: get(apiResponse, 'lastModified'),
+        name: get(apiResponse, 'name'),
+        heading: get(apiResponse, 'elements.heading.value'),
+        author: get(apiResponse, 'elements.author.value'),
+        body: get(apiResponse, 'elements.body.values', []),
+        imageUrl: apiDomain + get(apiResponse, 'elements.mainImage.value.leadImage.renditions.card.url'),
+      });
+    }));
   }
 }
